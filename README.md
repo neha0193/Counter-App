d878a3379735e4f104446e77cacb6313db427e95
neha0193 <neha031919@gmail.com>
2025-09-09 08:39:36 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## counter App 
+* installed react-icons and tailwind css 
+* learned about state update batching behaviour .. to solve this we use functional update in state update of use state hook 
+* also built dark-light mode functionality of tailwind css 
+* in VS it shows 2 problems in index.css bcuz of inetilsensecss is not yet update to v4 so there is no error in actual 
---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { FaLightbulb  } from "react-icons/fa";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [isDark, setIsDark] = useState(false);
+
+  return (
+    <div className={isDark ? "dark" : "" }>
+      <div className="bg-amber-400 dark:bg-primary-dark min-h-screen "> 
+        <h1 className=" dark:text-white text-4xl text-center pt-32 pb-10">
+          Counter App
+        </h1>
+        <p className="text-xl dark:text-white text-center">
+          Counter Value : {count}
+        </p>
+
+        <div className="text-center py-10">
+          <button
+            className="px-4 mx-2 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700 transition duration-200"
+            type="button"
+            onClick={() => setCount(0)}
+          >
+            Reset
+          </button>
+
+          <button
+            className="px-4 mx-2 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700 transition duration-200"
+            type="button"
+            onClick={() => setCount((prev) => prev + 1)}
+          >
+            Increment
+          </button>
+
+          <button
+            className="px-4 mx-2 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700 transition duration-200"
+            type="button"
+            onClick={() => setCount((prev) => prev - 1)}
+          >
+            Decrement
+          </button>
+        </div>
+        <div className="text-center">
+          <button type="button" onClick={() => setIsDark((prev) => !prev)}>
+            <FaLightbulb   className="dark:text-white text-5xl" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
